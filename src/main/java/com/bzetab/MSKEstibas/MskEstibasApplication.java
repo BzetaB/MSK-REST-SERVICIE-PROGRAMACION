2ce5db5ae66fc2756b1d1a2d6ab4a1ba44c9e46e
BzetaB <zevallosbenjamin100@gmail.com>
2024-11-18 23:41:48 -0500

    Creacion del proyecto

---
+++ src/main/java/com/bzetab/MSKEstibas/MskEstibasApplication.java
@@ -0,0 +1,13 @@
+package com.bzetab.MSKEstibas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MskEstibasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MskEstibasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bzetab/MSKEstibas/MskEstibasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzetab.MSKEstibas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MskEstibasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
