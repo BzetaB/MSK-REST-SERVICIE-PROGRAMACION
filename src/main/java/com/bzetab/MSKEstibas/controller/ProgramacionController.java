4eb9f9207ef3f4ade4520b967367fbb7b3447547
BzetaB <zevallosbenjamin100@gmail.com>
2024-12-19 03:55:00 -0500

    cambios subidos

--- src/main/java/com/bzetab/MSKEstibas/controller/ProgramacionController.java
+++ src/main/java/com/bzetab/MSKEstibas/controller/ProgramacionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/programacion")
+@RequestMapping("/wsmskestiba/programacion")
 @AllArgsConstructor
 public class ProgramacionController {
 
